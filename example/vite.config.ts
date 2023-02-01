7f855afa15b8728ffe222d846bb20c20c58dc3b7
Sean Dodson <dodsonsean@comcast.net>
2023-02-01 18:36:06 -0500

    add build, test, and deploy scripts

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: './src/pages',
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
   plugins: [react()],
 });
